2e5f2bdbdfe51446bd40e85ba20395ce99305750
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-04-26 13:50:08 +0200

    flakify

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "My talks";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            pandoc
+            (python3.withPackages (ps: with ps; [ pyyaml ]))
+          ];
+        };
+      }
+    );
+}
