e2f768d1f1ba2b32980d458eccb9dc11211efee2
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-06-12 17:02:36 +0200

    flake-parts

--- flake.nix
+++ flake.nix
@@ -3,38 +3,41 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
     laas-beamer-theme = {
       url = "git+https://gitlab.laas.fr/gsaurel/laas-beamer-theme";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
     };
   };
 
   outputs =
-    {
-      laas-beamer-theme,
-      nixpkgs,
-      flake-utils,
-      ...
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        talks = pkgs.callPackage ./default.nix {
-          laas-beamer-theme = laas-beamer-theme.packages.${system}.default;
+    inputs@{ laas-beamer-theme, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+      perSystem =
+        {
+          pkgs,
+          self',
+          system,
+          ...
+        }:
+        {
+          checks = {
+            packages = self'.packages.default;
+          };
+          packages.default = pkgs.callPackage ./default.nix {
+            laas-beamer-theme = laas-beamer-theme.packages.${system}.default;
+          };
+          devShells.default = pkgs.mkShell {
+            inputsFrom = [ self'.packages.default ];
+            packages = [
+              pkgs.pdfpc
+              pkgs.watchexec
+            ];
+          };
         };
-      in
-      {
-        packages.default = talks;
-        devShells.default = pkgs.mkShell {
-          inputsFrom = [ talks ];
-          packages = [
-            pkgs.pdfpc
-            pkgs.watchexec
-          ];
-        };
-      }
-    );
+    };
 }
