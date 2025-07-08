69504153c6d0969c7363b5c6ea5d5d6f015269b9
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-07-08 23:35:17 +0200

    icons: filter by slug

--- icons.js
+++ icons.js
@@ -2,11 +2,11 @@ const fs = require("fs");
 const path = require("path");
 const simpleIcons = require("simple-icons");
 
-const brands = ["Creative Commons", "GitHub", "GitLab"];
+const brands = ["creativecommons", "github", "gitlab"];
 const output = path.join(__dirname, "public");
 
 for (const icon of Object.values(simpleIcons)) {
-	if (brands.includes(icon.title)) {
+	if (brands.includes(icon.slug)) {
 		const filePath = path.join(output, `${icon.slug}.svg`);
 		const svg = icon.svg.replace(
 			"svg ",
